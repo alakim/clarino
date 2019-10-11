6f3a0245723e1c62a9ed88d53af54a49d7db62e5
alakim <alakm@rambler.ru>
2019-10-11 11:41:57 +0300

    Simple Python implementation

---
+++ python/clarino.py
@@ -0,0 +1,37 @@
+
+class Clarino:
+	@staticmethod
+	def tagContent(nm, content):
+		head = '<'+nm;
+		for e in content:
+			if type(e) is dict:
+				for n in e:
+					head+=' '+n+'="'+e[n]+'"'
+
+		tail = '</%s>'%nm;
+		# inner = ''.join(list(map(lambda e:e.__str__(), content)))
+		inner = ''
+		for e in content:
+			if not(type(e) is dict):
+				inner+=e.__str__()
+		return head+'>'+inner+tail
+
+	@staticmethod
+	def tag(nm):
+		#return lambda *x: '<%s>%s</%s>'%(nm, ''.join(list(map(lambda e:e.__str__(), x))), nm)
+		return lambda *x: Clarino.tagContent(nm, x)
+
+	@staticmethod
+	def repeat(n, templ):
+		s = ''
+		for i in range(n):
+			s = s+templ(i+1)
+		return s
+
+	@staticmethod
+	def apply(coll, templ):
+		s = ''
+		for e in coll:
+			s = s+templ(e)
+		return s
+	
---
+++ python/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#coding:utf8
+
+import sys;
+import os;
+from clarino import Clarino;
+
+ccc = ['a', 'b', 'c'];
+
+C = Clarino
+div = C.tag('div')
+p = C.tag('p')
+span = C.tag('span')
+
+print(div(
+	p({'class':'attention', 'id':'att1'}, 'abc'),
+	p('count: ', 5, ' items'),
+	C.repeat(2, lambda i:span('#'+i.__str__())),
+	C.apply(ccc, lambda e:div(e))
+))
+
+print 'Done';
