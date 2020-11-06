25e88d6c644573a8b51656df29f2a08a6c507ccb
alakim <alakm@ramble.ru>
2020-11-06 16:04:30 +0300

    Clarino module file

--- samples/forms/js/core.js
+++ samples/forms/js/core.js
@@ -1,6 +1,7 @@
-export const $C = Clarino.version('1.5.0');
+import Clarino from '../../../clarino.module.js';
+export const $C = Clarino.version('2.0.0');
 export const $H = $C.simple;
-export const {px,pc} = $C.css.unit;
+export const {px} = $C.css.unit;
 export const css = $C.css.keywords;
 export const {border} = $C.css.template;
 export const Locale = $C.enumeration('en;ru');
